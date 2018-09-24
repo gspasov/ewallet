de09cd10169158851fb69b2ae4d8d97195c244d0
gspasov <gvspasov@gmail.com>
2018-09-24 18:35:00 +0300

    [GH-1] Created empty mix ewallet project

---
+++ lib/ewallet.ex
@@ -0,0 +1,18 @@
+defmodule Ewallet do
+  @moduledoc """
+  Documentation for Ewallet.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ewallet.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
